7c42bebd0ed81d6824b096fe1275883bb8c272fb
FOUR 4to6 <39036561+four4to6@users.noreply.github.com>
2023-11-11 02:57:38 +0900

    Create kanto_region.js

---
+++ data/kanto_region.js
@@ -0,0 +1,51 @@
+﻿const kanto_region = [
+    {
+        "pref_name": "茨城県",
+        "pref_capital": "水戸市",
+        "lat": 36.341817,
+        "lon": 140.446796,
+        "population": 2867009
+    },
+    {
+        "pref_name": "栃木県",
+        "pref_capital": "宇都宮市",
+        "lat": 36.56575,
+        "lon": 139.883526,
+        "population": 1933146
+    },
+    {
+        "pref_name": "群馬県",
+        "pref_capital": "前橋市",
+        "lat": 36.391205,
+        "lon": 139.060917,
+        "population": 1939110
+    },
+    {
+        "pref_name": "埼玉県",
+        "pref_capital": "さいたま市",
+        "lat": 35.857771,
+        "lon": 139.647804,
+        "population": 7344765
+    },
+    {
+        "pref_name": "千葉県",
+        "pref_capital": "千葉市",
+        "lat": 35.604563,
+        "lon": 140.123179,
+        "population": 6284480
+    },
+    {
+        "pref_name": "東京都",
+        "pref_capital": "新宿区",
+        "lat": 35.689185,
+        "lon": 139.691648,
+        "population": 14047594
+    },
+    {
+        "pref_name": "神奈川県",
+        "pref_capital": "横浜市",
+        "lat": 35.447505,
+        "lon": 139.642347,
+        "population": 9237337
+    }
+];
