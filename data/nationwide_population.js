23c8240783def2eecb55e71003aefdcff5683c1f
FOUR 4to6 <39036561+four4to6@users.noreply.github.com>
2023-11-11 03:36:33 +0900

    Create nationwide_population.js

---
+++ data/nationwide_population.js
@@ -0,0 +1,331 @@
+﻿const nationwide = [
+    {
+        "pref_name": "北海道",
+        "pref_capital": "札幌市",
+        "lat": 43.064359,
+        "lon": 141.347449,
+        "population": 5225
+    },
+    {
+        "pref_name": "青森県",
+        "pref_capital": "青森市",
+        "lat": 40.824294,
+        "lon": 140.740054,
+        "population": 1238
+    },
+    {
+        "pref_name": "岩手県",
+        "pref_capital": "盛岡市",
+        "lat": 39.70353,
+        "lon": 141.152667,
+        "population": 1211
+    },
+    {
+        "pref_name": "宮城県",
+        "pref_capital": "仙台市",
+        "lat": 38.268737,
+        "lon": 140.872183,
+        "population": 2302
+    },
+    {
+        "pref_name": "秋田県",
+        "pref_capital": "秋田市",
+        "lat": 39.718175,
+        "lon": 140.103356,
+        "population": 960
+    },
+    {
+        "pref_name": "山形県",
+        "pref_capital": "山形市",
+        "lat": 38.240127,
+        "lon": 140.362533,
+        "population": 1068
+    },
+    {
+        "pref_name": "福島県",
+        "pref_capital": "福島市",
+        "lat": 37.750146,
+        "lon": 140.466754,
+        "population": 1833
+    },
+    {
+        "pref_name": "茨城県",
+        "pref_capital": "水戸市",
+        "lat": 36.341817,
+        "lon": 140.446796,
+        "population": 2867
+    },
+    {
+        "pref_name": "栃木県",
+        "pref_capital": "宇都宮市",
+        "lat": 36.56575,
+        "lon": 139.883526,
+        "population": 1933
+    },
+    {
+        "pref_name": "群馬県",
+        "pref_capital": "前橋市",
+        "lat": 36.391205,
+        "lon": 139.060917,
+        "population": 1939
+    },
+    {
+        "pref_name": "埼玉県",
+        "pref_capital": "さいたま市",
+        "lat": 35.857771,
+        "lon": 139.647804,
+        "population": 7345
+    },
+    {
+        "pref_name": "千葉県",
+        "pref_capital": "千葉市",
+        "lat": 35.604563,
+        "lon": 140.123179,
+        "population": 6284
+    },
+    {
+        "pref_name": "東京都",
+        "pref_capital": "新宿区",
+        "lat": 35.689185,
+        "lon": 139.691648,
+        "population": 14048
+    },
+    {
+        "pref_name": "神奈川県",
+        "pref_capital": "横浜市",
+        "lat": 35.447505,
+        "lon": 139.642347,
+        "population": 9237
+    },
+    {
+        "pref_name": "新潟県",
+        "pref_capital": "新潟市",
+        "lat": 37.901699,
+        "lon": 139.022728,
+        "population": 2201
+    },
+    {
+        "pref_name": "富山県",
+        "pref_capital": "富山市",
+        "lat": 36.695274,
+        "lon": 137.211302,
+        "population": 1035
+    },
+    {
+        "pref_name": "石川県",
+        "pref_capital": "金沢市",
+        "lat": 36.594729,
+        "lon": 136.62555,
+        "population": 1133
+    },
+    {
+        "pref_name": "福井県",
+        "pref_capital": "福井市",
+        "lat": 36.06522,
+        "lon": 136.221641,
+        "population": 767
+    },
+    {
+        "pref_name": "山梨県",
+        "pref_capital": "甲府市",
+        "lat": 35.665102,
+        "lon": 138.568985,
+        "population": 810
+    },
+    {
+        "pref_name": "長野県",
+        "pref_capital": "長野市",
+        "lat": 36.651282,
+        "lon": 138.180972,
+        "population": 2048
+    },
+    {
+        "pref_name": "岐阜県",
+        "pref_capital": "岐阜市",
+        "lat": 35.39116,
+        "lon": 136.722204,
+        "population": 1979
+    },
+    {
+        "pref_name": "静岡県",
+        "pref_capital": "静岡市",
+        "lat": 34.976987,
+        "lon": 138.383057,
+        "population": 3633
+    },
+    {
+        "pref_name": "愛知県",
+        "pref_capital": "名古屋市",
+        "lat": 35.180247,
+        "lon": 136.906698,
+        "population": 7542
+    },
+    {
+        "pref_name": "三重県",
+        "pref_capital": "津市",
+        "lat": 34.730547,
+        "lon": 136.50861,
+        "population": 1770
+    },
+    {
+        "pref_name": "滋賀県",
+        "pref_capital": "大津市",
+        "lat": 35.004532,
+        "lon": 135.868588,
+        "population": 1414
+    },
+    {
+        "pref_name": "京都府",
+        "pref_capital": "京都市",
+        "lat": 35.0209962,
+        "lon": 135.7531135,
+        "population": 2578
+    },
+    {
+        "pref_name": "大阪府",
+        "pref_capital": "大阪市",
+        "lat": 34.686492,
+        "lon": 135.518992,
+        "population": 8838
+    },
+    {
+        "pref_name": "兵庫県",
+        "pref_capital": "神戸市",
+        "lat": 34.69128,
+        "lon": 135.183087,
+        "population": 5465
+    },
+    {
+        "pref_name": "奈良県",
+        "pref_capital": "奈良市",
+        "lat": 34.685296,
+        "lon": 135.832745,
+        "population": 1324
+    },
+    {
+        "pref_name": "和歌山県",
+        "pref_capital": "和歌山市",
+        "lat": 34.224806,
+        "lon": 135.16795,
+        "population": 923
+    },
+    {
+        "pref_name": "鳥取県",
+        "pref_capital": "鳥取市",
+        "lat": 35.503463,
+        "lon": 134.238258,
+        "population": 553
+    },
+    {
+        "pref_name": "島根県",
+        "pref_capital": "松江市",
+        "lat": 35.472248,
+        "lon": 133.05083,
+        "population": 671
+    },
+    {
+        "pref_name": "岡山県",
+        "pref_capital": "岡山市",
+        "lat": 34.66132,
+        "lon": 133.934414,
+        "population": 1888
+    },
+    {
+        "pref_name": "広島県",
+        "pref_capital": "広島市",
+        "lat": 34.396033,
+        "lon": 132.459595,
+        "population": 2800
+    },
+    {
+        "pref_name": "山口県",
+        "pref_capital": "山口市",
+        "lat": 34.185648,
+        "lon": 131.470755,
+        "population": 1342
+    },
+    {
+        "pref_name": "徳島県",
+        "pref_capital": "徳島市",
+        "lat": 34.065732,
+        "lon": 134.559293,
+        "population": 720
+    },
+    {
+        "pref_name": "香川県",
+        "pref_capital": "高松市",
+        "lat": 34.34014,
+        "lon": 134.04297,
+        "population": 950
+    },
+    {
+        "pref_name": "愛媛県",
+        "pref_capital": "松山市",
+        "lat": 33.841649,
+        "lon": 132.76585,
+        "population": 1335
+    },
+    {
+        "pref_name": "高知県",
+        "pref_capital": "高知市",
+        "lat": 33.55969,
+        "lon": 133.530887,
+        "population": 692
+    },
+    {
+        "pref_name": "福岡県",
+        "pref_capital": "福岡市",
+        "lat": 33.606767,
+        "lon": 130.418228,
+        "population": 5135
+    },
+    {
+        "pref_name": "佐賀県",
+        "pref_capital": "佐賀市",
+        "lat": 33.249367,
+        "lon": 130.298822,
+        "population": 811
+    },
+    {
+        "pref_name": "長崎県",
+        "pref_capital": "長崎市",
+        "lat": 32.744542,
+        "lon": 129.873037,
+        "population": 1312
+    },
+    {
+        "pref_name": "熊本県",
+        "pref_capital": "熊本市",
+        "lat": 32.790385,
+        "lon": 130.742345,
+        "population": 1738
+    },
+    {
+        "pref_name": "大分県",
+        "pref_capital": "大分市",
+        "lat": 33.2382,
+        "lon": 131.612674,
+        "population": 1124
+    },
+    {
+        "pref_name": "宮崎県",
+        "pref_capital": "宮崎市",
+        "lat": 31.91109,
+        "lon": 131.423855,
+        "population": 1070
+    },
+    {
+        "pref_name": "鹿児島県",
+        "pref_capital": "鹿児島市",
+        "lat": 31.560219,
+        "lon": 130.557906,
+        "population": 1588
+    },
+    {
+        "pref_name": "沖縄県",
+        "pref_capital": "那覇市",
+        "lat": 26.211538,
+        "lon": 127.681115,
+        "population": 1467
+    }
+];
